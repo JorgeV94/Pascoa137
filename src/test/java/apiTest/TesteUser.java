17d7ba441d960f62c047ff767a4371fabe80de32
Jorge Vieira <jorge.vieira.a@hotmail.com>
2023-03-13 14:49:14 -0300

    API Rest-assured

---
+++ src/test/java/apiTest/TesteUser.java
@@ -0,0 +1,17 @@
+// nome do pacote
+package apiTest;
+
+// Bibliotecas
+import org.junit.jupiter.api.Test;
+
+// Classe
+public class TesteUser {
+    // Atributos
+
+    // Funções e Métodos
+    @Test
+    public void testarIncluirUser(){
+
+    }
+
+}
